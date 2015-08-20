a3edc10016fbc7ff234c79b08f65e78a0e2bd039
LeeBry <bylee@NN123-HA-16.HS.sd203.org>
2015-08-20 14:11:27 -0500

    Day1 of Coding
    Created 3 new turtles, and made then move around.

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,29 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        Turtle turtle2= new Turtle(turtleWorld);
+        Turtle turtle3= new Turtle(turtleWorld);
+        Turtle turtle4= new Turtle(turtleWorld);
+        turtle.setPenColor(Color.GREEN);
         turtle.penDown();
         turtle.forward(50);
+        turtle.turn(90);
+        turtle.forward(50);
+        turtle2.setPenColor(Color.BLUE);
+        turtle2.penDown();
+        turtle2.forward(50);
+        turtle2.turn(180);
+        turtle2.forward(50);
+        turtle3.setPenColor(Color.RED);
+        turtle3.penDown();
+        turtle3.forward(100);
+        turtle3.turn(270);
+        turtle3.forward(50);
+        turtle4.setPenColor(Color.BLACK);
+        turtle4.penDown();
+        turtle4.forward(100);
+        turtle4.turn(360);
+        turtle4.forward(50);
+       
     }
 }
