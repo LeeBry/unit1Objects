e902dca9054e15a5e501673779182c6663551773
LeeBry <bylee@NN123-HA-16.HS.sd203.org>
2015-08-21 14:11:50 -0500

    Random and color added
    Made it so the turtle moves in random (x,y) coordinates, and changes
    color everytime

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Random;
 
 public class TurtleWorld
 {
@@ -6,29 +7,26 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        Turtle turtle2= new Turtle(turtleWorld);
-        Turtle turtle3= new Turtle(turtleWorld);
-        Turtle turtle4= new Turtle(turtleWorld);
         turtle.setPenColor(Color.GREEN);
+        Random number = new Random();
+        number.nextInt(100);
+        
+  
         turtle.penDown();
         turtle.forward(50);
         turtle.turn(90);
         turtle.forward(50);
-        turtle2.setPenColor(Color.BLUE);
-        turtle2.penDown();
-        turtle2.forward(50);
-        turtle2.turn(180);
-        turtle2.forward(50);
-        turtle3.setPenColor(Color.RED);
-        turtle3.penDown();
-        turtle3.forward(100);
-        turtle3.turn(270);
-        turtle3.forward(50);
-        turtle4.setPenColor(Color.BLACK);
-        turtle4.penDown();
-        turtle4.forward(100);
-        turtle4.turn(360);
-        turtle4.forward(50);
+        turtle.turn(90);
+        turtle.forward(50);
+        turtle.turn(90);
+        turtle.forward(50);
+        turtle.setColor(Color.BLUE);
+            while (true){
+        turtle.moveTo(number.nextInt(),number.nextInt());
+        turtle.setColor(Color.RED);
+        turtle.moveTo(number.nextInt(),number.nextInt());
+        turtle.setColor(Color.GREEN);
+     
        
-    }
+    }}
 }
