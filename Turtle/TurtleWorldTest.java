8a4b3e2fc1d1e487d25ab7c986b1eb6eee22fb9a
LeeBry <Echoslee@yahoo.com>
2015-09-20 11:31:42 -0500

    Making polygons with turtle
    Makes the turtle make a polygon with 3-12 sides, in the color pink.
    Using while loops.

---
+++ Turtle/TurtleWorldTest.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+import java.util.Random;
+
+public class TurtleWorldTest
+{
+    public static void main(String args[])
+    {
+        World turtleWorld = new World();
+        Turtle turtle = new Turtle(turtleWorld);
+        turtle.setPenColor(Color.PINK);
+        Random gener = new Random();
+        int number= gener.nextInt(10)+3;
+        System.out.println(number);
+        int interior= (180*(number-2)/number);
+        int times= number;
+        while (times !=0)
+        {
+            turtle.forward(50);
+            turtle.turn(180-interior);
+            times-=1;
+      
+      
+    }
+}
+}
